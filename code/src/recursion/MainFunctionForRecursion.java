a5b0fd0eb4820e6d7ca3a63012c2bc42f7f49ee1
salunkhegr1712 <salunkhegr1712@gmail.com>
2025-11-18 23:51:51 +0530

    feat: add reverseString method to reverse a given string in MainFunctionForRecursion

--- code/src/recursion/MainFunctionForRecursion.java
+++ code/src/recursion/MainFunctionForRecursion.java
@@ -1,11 +1,24 @@
 package recursion;
 
+import java.util.Objects;
+
 public class MainFunctionForRecursion {
-    static void main() {
+
+    public static String reverseString(String input){
+        if(Objects.equals(input, "") || input.length()==1){
+            return input;
+        }
+        return reverseString(input.substring(1))+ input.charAt(0);
+    }
+
+
+    public static void main() {
         Fibonacci fibonacci=new Fibonacci();
         System.out.println(fibonacci.getListOfFibonacciList(11));
 
         Factorial factorial=new Factorial();
         System.out.println(factorial.getListOfFactorialList(11));
+
+        System.out.println(reverseString("abcd"));
     }
 }
